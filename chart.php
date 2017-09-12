6290a4c5a845601ab8da953c4b55e01ff904c0ea
Albritter <maxiritter@aol.com>
2017-09-12 12:55:34 +0200

    Zwischenstand

---
+++ chart.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="styles.css"/>
+<div class="chart-entry">
+    <div>
+        <div>
+            Artikelname
+        </div>
+        <div>
+            <img src="logo.png"/>
+        </div>
+    </div>
+    <div class="pricing">
+        <div>
+            anzahl
+        </div>
+        <div>
+            preis einzeld
+        </div>
+        <div>
+            preis gesamt
+        </div>
+    </div>
+</div>
