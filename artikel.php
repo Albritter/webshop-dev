ee288be8fde717df048e395f08a637354063838e
Albritter <maxiritter@aol.com>
2017-09-12 14:13:41 +0200

    Zwischenstand

--- artikel.php
+++ artikel.php
@@ -46,5 +46,48 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="row artikel">
+        <div class="col-md">
+            <div style="">Artikelname</div>
+            <div>
+                <img src="logo.png"/>
+                <div>Preis€</div>
+                <div>
+                    <form action="artikel.php" method="get">
+                        <input hidden value="nummer1" name="nummer"/>
+                        <input type="submit" value="In den Warenkorb"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md">
+            <div style="">Artikelname</div>
+            <div>
+                <img src="logo.png"/>
+                <div>Preis€</div>
+                <div>
+                    <form action="artikel.php" method="get">
+                        <input hidden value="nummer2" name="nummer"/>
+                        <input type="submit" value="In den Warenkorb"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md">
+            <div style="">Artikelname</div>
+            <div>
+                <img src="logo.png"/>
+                <div>Preis€</div>
+                <div>
+                    <form action="artikel.php" method="get">
+                        <input hidden value="nummer3" name="nummer"/>
+                        <input type="submit" value="In den Warenkorb"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- chart.php
+++ chart.php
@@ -1,6 +1,8 @@
 <link rel="stylesheet" href="styles.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+      integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 <div class="chart-entry">
-    <div>
+    <div class="description">
         <div>
             Artikelname
         </div>
@@ -8,7 +10,49 @@
             <img src="logo.png"/>
         </div>
     </div>
-    <div class="pricing">
+    <div class="pricing" style="float: right    ">
+        <div>
+            anzahl
+        </div>
+        <div>
+            preis einzeld
+        </div>
+        <div>
+            preis gesamt
+        </div>
+    </div>
+</div>
+<div class="chart-entry">
+    <div class="description">
+        <div>
+            Artikelname
+        </div>
+        <div>
+            <img src="logo.png"/>
+        </div>
+    </div>
+    <div class="pricing" style="float: right    ">
+        <div>
+            anzahl
+        </div>
+        <div>
+            preis einzeld
+        </div>
+        <div>
+            preis gesamt
+        </div>
+    </div>
+</div>
+<div class="chart-entry">
+    <div class="description">
+        <div>
+            Artikelname
+        </div>
+        <div>
+            <img src="logo.png"/>
+        </div>
+    </div>
+    <div class="pricing" style="float: right    ">
         <div>
             anzahl
         </div>
