26412035bd966c17e266b16fe9e6291208bb029a
Albritter <comit35/P>
2017-09-13 20:15:02 +0200

    zwischenstand

--- header.php
+++ header.php
@@ -8,24 +8,41 @@ ini_set('display_errors', 'On');
 <head>
 	<title>Webshop</title>
 	<link rel="stylesheet" href="styles.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
 </head>
 <body>
 
-<header class="header">
-	<h1>Webshop</h1>
-</header>
-<div class="menu">
-	<form method="get" action="search.php">
-		<ul>
-
-			<li><a href="index.php">Home</a></li>
-			<li><a href="artikel.php">Alle Artikel</a></li>
-			<li><a href="chart.php">Warenkorb</a></li>
-		</ul>
-		<div>
-			<input type="search" name="query">
-			<input type="submit" value="Suche">
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.php">WebSiteName</a>
 		</div>
-	</form>
-</div>
+		<ul class="nav navbar-nav">
+            <?php
+            switch (basename($_SERVER['SCRIPT_FILENAME'])) {
+                case "index.php":
+                    echo '<li class = "active" ><a href = "index.php" > Home</a ></li >';
+                    echo '<li ><a href = "artikel.php" > Alle Artikel </a ></li >';
+                    echo '<li ><a href = "chart.php" > Warenkorb</a ></li >';
+                    echo '<li ><a href = "https://github.com/albritter/webshop-dev" > SourceCode</a ></li >';
+                    break;
+                case "artikel.php":
+                    echo '<li ><a href = "index.php" > Home</a ></li >';
+                    echo '<li class = "active" ><a href = "artikel.php" > Alle Artikel </a ></li >';
+                    echo '<li ><a href = "chart.php" > Warenkorb</a ></li >';
+                    echo '<li ><a href = "https://github.com/albritter/webshop-dev" > SourceCode</a ></li >';
+                    break;
+                case "chart.php":
+                    echo '<li ><a href = "index.php" > Home</a ></li >';
+                    echo '<li ><a href = "artikel.php" > Alle Artikel </a ></li >';
+                    echo '<li class = "active" ><a href = "chart.php" > Warenkorb</a ></li >';
+                    echo '<li ><a href = "https://github.com/albritter/webshop-dev" > SourceCode</a ></li >';
+                    break;
+
+            } ?></ul>
+	</div>
+</nav>
