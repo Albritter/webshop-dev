f7c77767f1545ab78c4b5b0b692dddf673efa64f
Albritter <comit35/P>
2017-11-12 22:02:04 +0100

    Neue Datenbank version mit der stored procedure
    MySQL Workbench meinte kommentarfelder einfügen zu müssen. Daher ist
    der Commit recht groß

--- database.sql
+++ database.sql
@@ -10,11 +10,12 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema db_webshop
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `db_webshop` ;
 
 -- -----------------------------------------------------
 -- Schema db_webshop
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `db_webshop` DEFAULT CHARACTER SET latin1 ;
+CREATE SCHEMA IF NOT EXISTS `db_webshop` DEFAULT CHARACTER SET utf8 ;
 USE `db_webshop` ;
 
 -- -----------------------------------------------------
@@ -23,18 +24,18 @@ USE `db_webshop` ;
 DROP TABLE IF EXISTS `db_webshop`.`article` ;
 
 CREATE TABLE IF NOT EXISTS `db_webshop`.`article` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(45) CHARACTER SET 'utf8' NOT NULL COMMENT '',
-  `descshort` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL COMMENT '',
-  `price` VARCHAR(45) CHARACTER SET 'utf8' NULL DEFAULT NULL COMMENT '',
-  `img` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL COMMENT '',
-  `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
-  `desclong` TEXT CHARACTER SET 'utf8' NULL DEFAULT NULL COMMENT '',
-  PRIMARY KEY (`id`, `name`)  COMMENT '')
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 45
-  DEFAULT CHARACTER SET = utf8
-  COLLATE = utf8_bin;
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) CHARACTER SET 'utf8' NOT NULL,
+  `descshort` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `price` VARCHAR(45) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `img` VARCHAR(255) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `desclong` TEXT CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `name`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 45
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 
 -- -----------------------------------------------------
@@ -43,13 +44,13 @@ CREATE TABLE IF NOT EXISTS `db_webshop`.`article` (
 DROP TABLE IF EXISTS `db_webshop`.`artikel` ;
 
 CREATE TABLE IF NOT EXISTS `db_webshop`.`artikel` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(255) NOT NULL COMMENT '',
-  `preis` DOUBLE NOT NULL COMMENT '',
-  `bild` VARCHAR(255) NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-  ENGINE = InnoDB
-  DEFAULT CHARACTER SET = latin1;
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `preis` DOUBLE NOT NULL,
+  `bild` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
@@ -58,16 +59,16 @@ CREATE TABLE IF NOT EXISTS `db_webshop`.`artikel` (
 DROP TABLE IF EXISTS `db_webshop`.`user` ;
 
 CREATE TABLE IF NOT EXISTS `db_webshop`.`user` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(45) NOT NULL COMMENT '',
-  `password` VARCHAR(60) NOT NULL COMMENT '',
-  `lastlogin` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
-  `lastloginsource` VARCHAR(45) NULL DEFAULT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC)  COMMENT '')
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 5
-  DEFAULT CHARACTER SET = latin1;
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(60) NOT NULL,
+  `lastlogin` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastloginsource` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
@@ -76,17 +77,17 @@ CREATE TABLE IF NOT EXISTS `db_webshop`.`user` (
 DROP TABLE IF EXISTS `db_webshop`.`sessions` ;
 
 CREATE TABLE IF NOT EXISTS `db_webshop`.`sessions` (
-  `idsession` VARCHAR(64) NOT NULL COMMENT '',
-  `user` INT(11) NULL DEFAULT NULL COMMENT '',
-  PRIMARY KEY (`idsession`)  COMMENT '',
-  INDEX `iduser_idx` (`user` ASC)  COMMENT '',
+  `idsession` VARCHAR(64) NOT NULL,
+  `user` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`idsession`),
+  INDEX `iduser_idx` (`user` ASC),
   CONSTRAINT `iduser`
-  FOREIGN KEY (`user`)
-  REFERENCES `db_webshop`.`user` (`id`)
+    FOREIGN KEY (`user`)
+    REFERENCES `db_webshop`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-  ENGINE = InnoDB
-  DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -95,42 +96,53 @@ CREATE TABLE IF NOT EXISTS `db_webshop`.`sessions` (
 DROP TABLE IF EXISTS `db_webshop`.`chart` ;
 
 CREATE TABLE IF NOT EXISTS `db_webshop`.`chart` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
-  `idsession` VARCHAR(45) NOT NULL COMMENT '',
-  `idarticle` INT(11) NOT NULL COMMENT '',
-  `count` INT(11) NOT NULL COMMENT
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `id_session_idx` (`idsession` ASC)  COMMENT '',
-  INDEX `id_article_idx` (`idarticle` ASC)  COMMENT '',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `idsession` VARCHAR(45) NOT NULL,
+  `idarticle` INT(11) NOT NULL,
+  `count` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_session_idx` (`idsession` ASC),
+  INDEX `id_article_idx` (`idarticle` ASC),
   CONSTRAINT `id_article`
-  FOREIGN KEY (`idarticle`)
-  REFERENCES `db_webshop`.`article` (`id`)
+    FOREIGN KEY (`idarticle`)
+    REFERENCES `db_webshop`.`article` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `id_session`
-  FOREIGN KEY (`idsession`)
-  REFERENCES `db_webshop`.`sessions` (`idsession`)
+    FOREIGN KEY (`idsession`)
+    REFERENCES `db_webshop`.`sessions` (`idsession`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 177
-  DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB
+AUTO_INCREMENT = 184
+DEFAULT CHARACTER SET = utf8;
 
+
+
+
+USE `db_webshop` ;
 
 -- -----------------------------------------------------
--- Table `db_webshop`.`test`
+-- procedure add_article_to_chart
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `db_webshop`.`test` ;
 
-CREATE TABLE IF NOT EXISTS `db_webshop`.`test` (
-  `id` INT(11) NOT NULL COMMENT '',
-  `text` VARCHAR(255) NOT NULL COMMENT '',
-  `text2` VARCHAR(255) NOT NULL COMMENT '',
-  `text3` TEXT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-  ENGINE = InnoDB
-  DEFAULT CHARACTER SET = latin1;
+USE `db_webshop`;
+DROP procedure IF EXISTS `db_webshop`.`add_article_to_chart`;
 
+DELIMITER $$
+USE `db_webshop`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_article_to_chart`(IN sessionid varchar(45),IN article INT(11))
+BEGIN
+
+set @c = (SELECT count(*) from chart where idsession = sessionid AND idarticle = article);
+
+if(IFNULL(@c,0) >= 1) THEN 
+	UPDATE chart set count = count +1 where idsession = sessionid AND idarticle = article ;
+ELSE INSERT INTO chart (idsession,idarticle,count) values(sessionid,article,0) ;
+END IF; 
+END$$
+
+DELIMITER ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
